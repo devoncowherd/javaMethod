51fd12cb14b66d51c052c16415d09f7abc621db0
Devon <71043147+devoncowherd@users.noreply.github.com>
2022-04-25 15:49:40 -0700

    -restructured

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package javaMethod;
+
+{
+    public static void main(String[] args)
+    {
+        String hello = "You thought this was going to print, 'Hello!'";
+
+        System.out.println(hello);
+    }
+}
